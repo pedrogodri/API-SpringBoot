7f752e00b0f8c66029704cc550445fea8143a8e9
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-06-25 00:34:20 -0300

    commit

--- api/src/main/java/br/com/projeto/api/model/Pessoa.java
+++ api/src/main/java/br/com/projeto/api/model/Pessoa.java
@@ -1,10 +1,27 @@
 package br.com.projeto.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Pessoas")
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigo;
     private String nome;
     private Integer idade;
     
+    public Integer getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
     public String getNome() {
         return nome;
     }
