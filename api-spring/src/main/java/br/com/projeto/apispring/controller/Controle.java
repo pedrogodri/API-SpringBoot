91304102a67021b391f98546162cea81829b801c
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-06-25 23:04:22 -0300

    commit

--- api-spring/src/main/java/br/com/projeto/apispring/controller/Controle.java
+++ api-spring/src/main/java/br/com/projeto/apispring/controller/Controle.java
@@ -1,11 +1,16 @@
 package br.com.projeto.apispring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
 import br.com.projeto.apispring.model.Pessoa;
 import br.com.projeto.apispring.repository.Repositorio;
@@ -15,6 +20,32 @@ public class Controle {
     
     @Autowired
     private Repositorio acao;
+
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa p) {
+        return acao.save(p);
+    }
+
+    @GetMapping("/api")
+    public List<Pessoa> selecionar() {
+        return acao.findAll();
+    }
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarCodigo(@PathVariable Integer codigo) {
+        return acao.findByCodigo(codigo);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa p) {
+        return acao.save(p);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable Integer codigo) {
+        Pessoa p = selecionarCodigo(codigo);
+        acao.delete(p);
+    }
 
     @GetMapping("")
     public String mensagem() {
--- api-spring/src/main/java/br/com/projeto/apispring/repository/Repositorio.java
+++ api-spring/src/main/java/br/com/projeto/apispring/repository/Repositorio.java
@@ -1,5 +1,7 @@
 package br.com.projeto.apispring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.com.projeto.apispring.model.Pessoa;
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
     
+    List<Pessoa> findAll();
+    Pessoa findByCodigo(Integer codigo);
 }
